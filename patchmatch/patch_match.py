1e83f41f6c2242bfa37e2ba97ca2860c1429e88f
Robert Schriver <schriver@rancor.csh.rit.edu>
2011-10-25 22:13:11 -0400

    Added multidimesional patch sizes

--- patchmatch/patch_match.py
+++ patchmatch/patch_match.py
@@ -13,7 +13,8 @@ from numpy.linalg import *
 
 
 def default_patch_difference(first_patch , second_patch):
-	#print first_patch , second_patch
+	#print first_patch 
+	#print second_patch
 	
 	diff = first_patch - second_patch
 	mse = multiply(diff , diff)
@@ -61,8 +62,13 @@ def nearest_neighbor_field(first_image , second_image , difference_function=defa
 				second_image_offset = nnf[x , y]
 
 				#print second_image_offset[0] , second_image_offset[1]
-				
-				second_image_patch = second_image[(second_image_offset[0] , second_image_offset[1])]
+				second_x_from = second_image_offset[0] - x_patch_offset
+				second_x_to = second_image_offset[0] + (x_patch_offset + 1)
+
+				second_y_from = second_image_offset[1] - y_patch_offset
+				second_y_to = second_image_offset[1] + (y_patch_offset + 1)
+
+				second_image_patch = second_image[second_x_from:second_x_to , second_y_from:second_y_to]
 				patch_difference = difference_function(first_image_patch , second_image_patch)
 
 				#print 'Current patch difference ' , patch_difference
@@ -89,8 +95,14 @@ def nearest_neighbor_field(first_image , second_image , difference_function=defa
 					previous_x_offset = nnf[x - 1 , y]
 					previous_y_offset = nnf[x , y - 1]
 
-					if previous_x_offset[0] + 1 < size(second_image , axis=0):
-						previous_x_patch = second_image[(previous_x_offset[0] + 1 , previous_x_offset[1])]
+					if previous_x_offset[0] + x_patch_offset + 1 < size(second_image , axis=0):
+						previous_x_from = previous_x_offset[0] - x_patch_offset + 1
+						previous_x_to = previous_x_offset[0] + (x_patch_offset + 1) + 1
+
+						previous_y_from = previous_x_offset[1] - y_patch_offset 
+						previous_y_to = previous_x_offset[1] + (y_patch_offset + 1)
+
+						previous_x_patch = second_image[previous_x_from:previous_x_to , previous_y_from:previous_y_to]
 
 						#print previous_x_patch
 						#print first_image_patch , '\n'
@@ -98,8 +110,14 @@ def nearest_neighbor_field(first_image , second_image , difference_function=defa
 						previous_x_patch_difference = difference_function(first_image_patch , previous_x_patch)
 
 
-					if previous_y_offset[1] + 1 < size(second_image , axis=1):
-						previous_y_patch = second_image[(previous_y_offset[0] , previous_y_offset[1] + 1)]
+					if previous_y_offset[1] + y_patch_offset + 1 < size(second_image , axis=1):
+						previous_x_from = previous_y_offset[0] - x_patch_offset
+						previous_x_to = previous_y_offset[0] + (x_patch_offset + 1)
+
+						previous_y_from = previous_y_offset[1] - y_patch_offset + 1
+						previous_y_to = previous_y_offset[1] + (y_patch_offset + 1) + 1
+
+						previous_y_patch = second_image[previous_x_from:previous_x_to , previous_y_from:previous_y_to]
 						
 						#print previous_y_patch
 						#print first_image_patch , '\n'
@@ -124,24 +142,34 @@ def nearest_neighbor_field(first_image , second_image , difference_function=defa
 				a = .5
 	
 				search_radius = second_image.shape[0] * pow(a , i) 				
+				#search_radius = 0
 
 				while search_radius > 1:
 					x_search_offset = round(second_image_offset[0] + search_radius * uniform(-1,1))
 					y_search_offset = round(second_image_offset[1] + search_radius * uniform(-1,1))
 
-					if x_search_offset < 0:
-						x_search_offset = 0
+					if x_search_offset < x_patch_offset:
+						x_search_offset = x_patch_offset
 					
-					if x_search_offset > second_image.shape[0] - 1:
-						x_search_offset = second_image.shape[0] -1
+					if x_search_offset > second_image.shape[0] - (x_patch_offset + 1):
+						x_search_offset = second_image.shape[0] - (x_patch_offset + 1)
 
-					if y_search_offset < 0:
-						y_search_offset = 0
+					if y_search_offset < y_patch_offset:
+						y_search_offset = y_patch_offset
 
-					if y_search_offset > second_image.shape[1] - 1:
-						y_search_offset = second_image.shape[1] - 1	
-					
-					second_image_patch = second_image[(x_search_offset , y_search_offset)]
+					if y_search_offset > second_image.shape[1] - (y_patch_offset + 1):
+						y_search_offset = second_image.shape[1] - (y_patch_offset + 1)	
+				
+					x_search_from = x_search_offset - x_patch_offset
+					x_search_to = x_search_offset + x_patch_offset + 1
+
+					y_search_from = y_search_offset - y_patch_offset
+					y_search_to = y_search_offset + y_patch_offset + 1
+
+					#print x_search_from , x_search_to
+					#print y_search_from , y_search_to
+
+					second_image_patch = second_image[x_search_from:x_search_to , y_search_from:y_search_to]
 
 					search_patch_difference = difference_function(first_image_patch , second_image_patch)
 					
@@ -158,16 +186,6 @@ def nearest_neighbor_field(first_image , second_image , difference_function=defa
 				#Now we are done propogating and iterating, we store the lowest patch offset and move on
 				nnf[x , y] = second_image_offset
 		print j
-
-		nnf_rgb = zeros((nnf.shape[0] , nnf.shape[1] , 3))
-		nnf_rgb[: , : , 0] = nnf[: , : , 0]
-		nnf_rgb[: , : , 1] = nnf[: , : , 1]
-		
-		imsave('nnf_' + str(j) + '.tif' , nnf_rgb)
-		
-		fixed = apply_nnf(nnf , second_image)
-
-		imsave('fixed_' + str(j) + '.tif' , fixed)
 
 	return nnf
 
@@ -191,15 +209,34 @@ def apply_nnf(nnf , original):
 	return shifted
 
 lena_un = lena()
-lena_shifted = generic_filter(lena_un , shift_filter , (151 , 151) , mode='wrap')
-nnf = nearest_neighbor_field(lena_un , lena_shifted)
+
+lena_shifted = generic_filter(lena_un , shift_filter , (101 , 101) , mode='wrap')
+print lena_shifted
+
+nnf = nearest_neighbor_field(lena_shifted , lena_un , patch_size=(1,1))
 
 print nnf
-print nnf[: , : , 1]
-print nnf.shape
 
-fixed = apply_nnf(nnf , lena_shifted)
+fixed_sh = apply_nnf(nnf , lena_un)
+
+diff_sh = fixed_sh - lena_un
 
 imsave('lena_un.tif' , lena_un)
 imsave('lena_sh.tif' , lena_shifted)
-imsave('fixed.tif' , fixed)
+imsave('fixed_sh.tif' , fixed_sh)
+imsave('diff_sh.tif' , diff_sh)
+
+lena_hole = lena()
+lena_hole[100:200 , 100:200] = 0
+
+nnf = nearest_neighbor_field(lena_un , lena_hole , patch_size=(7,7))
+
+print nnf
+
+fixed_hole = apply_nnf(nnf , lena_un)
+
+diff_hole = fixed_hole - lena_un
+
+imsave('lena_hole.tif' , lena_hole)
+imsave('fixed_hole.tif' , fixed_hole)
+imsave('diff_hole.tif' , diff_hole)
