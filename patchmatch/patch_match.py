05747a6ce0a9b20be7f90f8d06b3346404ce7362
Robert Schriver <schriver@rancor.csh.rit.edu>
2011-10-31 14:04:43 -0400

    Added averaging of NNF values. Prettyr sure the NNF algorithm is wrong, just wanted to commit with some actual results

--- patchmatch/patch_match.py
+++ patchmatch/patch_match.py
@@ -13,12 +13,16 @@ from numpy.linalg import *
 
 
 def default_patch_difference(first_patch , second_patch):
-	#print first_patch 
-	#print second_patch
-	
-	diff = first_patch - second_patch
+	#print 'First ' , first_patch 
+	#print 'Second ' , second_patch
+
+	first_patch_32 = int32(first_patch)
+	second_patch_32 = int32(second_patch)
+
+	diff = subtract(first_patch_32 , second_patch_32)
+	#print 'Diff ' , diff
 	mse = multiply(diff , diff)
-	#print mse , '\n'
+	#print 'MSE ' , mse, '\n'
 	return mse.sum()
 
 def initialize_nearest_neighbor_field(nnf_size , patch_size):
@@ -33,9 +37,179 @@ def initialize_nearest_neighbor_field(nnf_size , patch_size):
 	#	for j in range(nnf_size[1]):
 	#		nnf[i , j , : , :] = ones(patch_size) * random_integers(0 , nnf_size[0] * nnf_size[1])
 	
-	print nnf
+	#print nnf
 
 	return nnf
+def update_nearest_neighbor_field(first_image , second_image , nnf , difference_function , patch_size):
+	for y in range(patch_size[1]  , first_image.shape[1] - patch_size[1]):
+		for x in range(patch_size[0] , first_image.shape[0] - patch_size[0]):
+			x_patch_offset = floor(patch_size[0] / 2)
+				
+			first_x_from = x - x_patch_offset
+			first_x_to = x + x_patch_offset + 1
+
+			y_patch_offset = floor(patch_size[1] / 2)
+
+			first_y_from = y - y_patch_offset
+			first_y_to = y + y_patch_offset + 1
+
+			first_image_patch = first_image[first_x_from:first_x_to , first_y_from:first_y_to]
+							
+			second_image_offset = nnf[x , y]
+
+			#print second_image_offset[0] , second_image_offset[1]
+			second_x_from = second_image_offset[0] - x_patch_offset
+			second_x_to = second_image_offset[0] + (x_patch_offset + 1)
+
+			second_y_from = second_image_offset[1] - y_patch_offset
+			second_y_to = second_image_offset[1] + (y_patch_offset + 1)
+
+			second_image_patch = second_image[second_x_from:second_x_to , second_y_from:second_y_to]
+			patch_difference = difference_function(first_image_patch , second_image_patch)
+
+			#print 'Current patch difference ' , patch_difference
+
+			if patch_difference < 4 * len(patch_size):
+				continue	
+
+			previous_x_patch_difference = patch_difference 
+			previous_y_patch_difference = patch_difference
+
+			#First, we check if the pixels above or below us have a better match than us.
+			if x > 0 and y > 0:
+				previous_x_offset = nnf[x - 1 , y]
+				previous_y_offset = nnf[x , y - 1]
+
+				if previous_x_offset[0] + x_patch_offset + 1 < size(second_image , axis=0):
+					previous_x_from = previous_x_offset[0] - x_patch_offset + 1
+					previous_x_to = previous_x_offset[0] + (x_patch_offset + 1) + 1
+
+					previous_y_from = previous_x_offset[1] - y_patch_offset 
+					previous_y_to = previous_x_offset[1] + (y_patch_offset + 1)
+
+					previous_x_patch = second_image[previous_x_from:previous_x_to , previous_y_from:previous_y_to]
+
+					previous_x_patch_difference = difference_function(first_image_patch , previous_x_patch)
+
+
+				if previous_y_offset[1] + y_patch_offset + 1 < size(second_image , axis=1):
+					previous_x_from = previous_y_offset[0] - x_patch_offset
+					previous_x_to = previous_y_offset[0] + (x_patch_offset + 1)
+
+					previous_y_from = previous_y_offset[1] - y_patch_offset + 1
+					previous_y_to = previous_y_offset[1] + (y_patch_offset + 1) + 1
+
+					previous_y_patch = second_image[previous_x_from:previous_x_to , previous_y_from:previous_y_to]
+						
+					previous_y_patch_difference = difference_function(first_image_patch , previous_y_patch)
+
+				if previous_x_patch_difference < patch_difference:
+					second_image_offset = [previous_x_offset[0] + 1, previous_x_offset[1]]
+					patch_difference = previous_x_patch_difference
+
+				if previous_y_patch_difference < patch_difference:
+					second_image_offset = [previous_y_offset[0] , previous_y_offset[1] + 1]
+					patch_difference = previous_y_patch_difference
+
+			#Now we are done propogating, or not, so we do a random search of our area for better matches
+
+			i = 0
+			a = .5
+	
+			search_radius = second_image.shape[0] * pow(a , i) 				
+			#search_radius = 0
+
+			while search_radius > 1:
+				x_search_offset = round(second_image_offset[0] + search_radius * uniform(-1,1))
+				y_search_offset = round(second_image_offset[1] + search_radius * uniform(-1,1))
+
+				if x_search_offset < x_patch_offset:
+					x_search_offset = x_patch_offset
+					
+				if x_search_offset > second_image.shape[0] - (x_patch_offset + 1):
+					x_search_offset = second_image.shape[0] - (x_patch_offset + 1)
+
+				if y_search_offset < y_patch_offset:
+					y_search_offset = y_patch_offset
+
+				if y_search_offset > second_image.shape[1] - (y_patch_offset + 1):
+					y_search_offset = second_image.shape[1] - (y_patch_offset + 1)	
+				
+				x_search_from = x_search_offset - x_patch_offset
+				x_search_to = x_search_offset + x_patch_offset + 1
+
+				y_search_from = y_search_offset - y_patch_offset
+				y_search_to = y_search_offset + y_patch_offset + 1
+
+				second_image_patch = second_image[x_search_from:x_search_to , y_search_from:y_search_to]
+
+				search_patch_difference = difference_function(first_image_patch , second_image_patch)
+					
+				if search_patch_difference < patch_difference:
+					second_image_offset = [x_search_offset , y_search_offset]
+					patch_difference = search_patch_difference
+
+				i += 1
+					
+				search_radius = second_image.shape[0] * pow(a , i) 				
+	
+				#print 'Patch difference after random search ' , patch_difference , '\n'
+			
+			#Now we are done propogating and iterating, we store the lowest patch offset and move on
+			nnf[x , y] = second_image_offset
+
+	return nnf
+
+def update_image(nnf , image ,  patch_size):
+	updated_image = image
+
+	for y in range(0 , image.shape[1]):
+		for x in range(0 , image.shape[0]):
+			x_offset = int(floor(patch_size[0] / 2))
+
+			x_from = -1 * x_offset
+			x_to = x_offset + 1
+
+
+			y_offset = int(floor(patch_size[1] / 2))
+			y_from = -1 * y_offset
+			y_to = y_offset + 1
+
+			image_average = 0
+
+			for i in range(x_from , x_to):
+				for j in range(y_from , y_to):
+					x_index = x + i
+
+					if x_index < 0:
+						x_index = 0
+
+					if x_index > image.shape[0] - 1:
+						x_index = image.shape[0] - 1
+
+					y_index = y + j
+
+					if y_index < 0:
+						y_index = 0
+
+					if y_index > image.shape[1] - 1:
+						y_index = image.shape[1] - 1
+					
+
+					image_offset = nnf[x_index , y_index]
+
+					image_value = image[image_offset[0] , image_offset[1]]
+				
+					image_average += image_value
+
+			image_average /= patch_size[0] * patch_size[1]
+			
+			nnf_offset = nnf[x , y]
+			#print image[nnf_offset[0] , nnf_offset[1]] , image_average
+
+			updated_image[x , y] = image_average
+
+	return updated_image
 
 def nearest_neighbor_field(first_image , second_image , difference_function=default_patch_difference , patch_size=(1 , 1)):
 	patch_difference_threshold = 10	
@@ -43,149 +217,31 @@ def nearest_neighbor_field(first_image , second_image , difference_function=defa
 	nnf = initialize_nearest_neighbor_field(first_image.shape , patch_size)
 	
 	print patch_size
-	for j in range(5):
-		#TODO: change these to maxint
-		for y in range(patch_size[1]  , first_image.shape[1] - patch_size[1]):
-			for x in range(patch_size[0] , first_image.shape[0] - patch_size[0]):
-				x_patch_offset = floor(patch_size[0] / 2)
-				
-				first_x_from = x - x_patch_offset
-				first_x_to = x + x_patch_offset + 1
-
-				y_patch_offset = floor(patch_size[1] / 2)
-
-				first_y_from = y - y_patch_offset
-				first_y_to = y + y_patch_offset + 1
-
-				first_image_patch = first_image[first_x_from:first_x_to , first_y_from:first_y_to]
-							
-				second_image_offset = nnf[x , y]
-
-				#print second_image_offset[0] , second_image_offset[1]
-				second_x_from = second_image_offset[0] - x_patch_offset
-				second_x_to = second_image_offset[0] + (x_patch_offset + 1)
-
-				second_y_from = second_image_offset[1] - y_patch_offset
-				second_y_to = second_image_offset[1] + (y_patch_offset + 1)
-
-				second_image_patch = second_image[second_x_from:second_x_to , second_y_from:second_y_to]
-				patch_difference = difference_function(first_image_patch , second_image_patch)
-
-				#print 'Current patch difference ' , patch_difference
-
-				"""if patch_difference < patch_difference_threshold:
-					#Propogation. This isn't exactly how it was described in the paper, but it should be pretty close?
-					#Could check here if their current difference is less than what we would be replacing it with
-					nnf[x - 1 , y] = second_image_offset
-					nnf[x , y - 1] = second_image_offset
-				else:
-					nnf[x , y , 0] = random_integers(patch_size[0] , nnf_size[0] - patch_size[0] , nnf_size)
-					nnf[x , y , 1] = random_integers(patch_size[1] , nnf_size[1] - patch_size[1] , nnf_size)
-				"""
-
-				if patch_difference < 10:
-					continue
-					
-
-				previous_x_patch_difference = patch_difference 
-				previous_y_patch_difference = patch_difference
-
-				#First, we check if the pixels above or below us have a better match than us.
-				if x > 0 and y > 0:
-					previous_x_offset = nnf[x - 1 , y]
-					previous_y_offset = nnf[x , y - 1]
-
-					if previous_x_offset[0] + x_patch_offset + 1 < size(second_image , axis=0):
-						previous_x_from = previous_x_offset[0] - x_patch_offset + 1
-						previous_x_to = previous_x_offset[0] + (x_patch_offset + 1) + 1
-
-						previous_y_from = previous_x_offset[1] - y_patch_offset 
-						previous_y_to = previous_x_offset[1] + (y_patch_offset + 1)
-
-						previous_x_patch = second_image[previous_x_from:previous_x_to , previous_y_from:previous_y_to]
-
-						#print previous_x_patch
-						#print first_image_patch , '\n'
-
-						previous_x_patch_difference = difference_function(first_image_patch , previous_x_patch)
-
-
-					if previous_y_offset[1] + y_patch_offset + 1 < size(second_image , axis=1):
-						previous_x_from = previous_y_offset[0] - x_patch_offset
-						previous_x_to = previous_y_offset[0] + (x_patch_offset + 1)
-
-						previous_y_from = previous_y_offset[1] - y_patch_offset + 1
-						previous_y_to = previous_y_offset[1] + (y_patch_offset + 1) + 1
-
-						previous_y_patch = second_image[previous_x_from:previous_x_to , previous_y_from:previous_y_to]
-						
-						#print previous_y_patch
-						#print first_image_patch , '\n'
-
-						previous_y_patch_difference = difference_function(first_image_patch , previous_y_patch)
-
-					if previous_x_patch_difference < patch_difference:
-						second_image_offset = previous_x_offset
-						patch_difference = previous_x_patch_difference
-
-					if previous_y_patch_difference < patch_difference:
-						second_image_offset = previous_y_offset
-						patch_difference = previous_y_patch_difference
-
-				#print 'Patch difference after propogation ' , patch_difference
-										
-				#Set our nnf to the new min value (Could just be the old value)
-				#nnf[x , y] = second_image_offset
-				#Now we are done propogating, or not, so we do a random search of our area for better matches
-
-				i = 0
-				a = .5
 	
-				search_radius = second_image.shape[0] * pow(a , i) 				
-				#search_radius = 0
 
-				while search_radius > 1:
-					x_search_offset = round(second_image_offset[0] + search_radius * uniform(-1,1))
-					y_search_offset = round(second_image_offset[1] + search_radius * uniform(-1,1))
 
-					if x_search_offset < x_patch_offset:
-						x_search_offset = x_patch_offset
-					
-					if x_search_offset > second_image.shape[0] - (x_patch_offset + 1):
-						x_search_offset = second_image.shape[0] - (x_patch_offset + 1)
+	#second_image = imresize(second_image , second_image.shape[0] / 8 , second_image.shape[1] / 8)
 
-					if y_search_offset < y_patch_offset:
-						y_search_offset = y_patch_offset
+	#for i in (1 , 2):
+	#	second_image = imresize(second_image , ((second_image.shape[0] / 2) * i , (second_image.shape[1] / 2) * i))
 
-					if y_search_offset > second_image.shape[1] - (y_patch_offset + 1):
-						y_search_offset = second_image.shape[1] - (y_patch_offset + 1)	
-				
-					x_search_from = x_search_offset - x_patch_offset
-					x_search_to = x_search_offset + x_patch_offset + 1
+	for j in range(5):
+		nnf = update_nearest_neighbor_field(first_image , second_image , nnf , difference_function , patch_size)
+			
+		nnf_rgb = zeros((nnf.shape[0] , nnf.shape[1] , 3))
+		nnf_rgb[: , : , 0] = nnf[: , : , 0]
+		nnf_rgb[: , : , 1] = nnf[: , : , 1]
 
-					y_search_from = y_search_offset - y_patch_offset
-					y_search_to = y_search_offset + y_patch_offset + 1
+		imsave('nnf_' + str(j) + '_' + '.tif' , nnf_rgb)
+		
 
-					#print x_search_from , x_search_to
-					#print y_search_from , y_search_to
+		second_image = update_image(nnf , second_image , patch_size)
 
-					second_image_patch = second_image[x_search_from:x_search_to , y_search_from:y_search_to]
+		imsave('fixed_' + str(j) + '.tif' , second_image)
 
-					search_patch_difference = difference_function(first_image_patch , second_image_patch)
-					
-					if search_patch_difference < patch_difference:
-						second_image_offset = [x_search_offset , y_search_offset]
-						patch_difference = search_patch_difference
-
-					i += 1
-					
-					search_radius = second_image.shape[0] * pow(a , i) 				
-
-					
-				#print 'Patch difference after random search ' , patch_difference , '\n'
-				#Now we are done propogating and iterating, we store the lowest patch offset and move on
-				nnf[x , y] = second_image_offset
 		print j
+
+			
 
 	return nnf
 
@@ -208,6 +264,30 @@ def apply_nnf(nnf , original):
 	
 	return shifted
 
+im_one = imread('camel.jpg' , flatten=1)
+im_two = imread('camel.jpg' , flatten=1)
+
+#im_one = imresize(im_one , (im_one.shape[0] / 8 , im_one.shape[1] / 8))
+#im_two = imresize(im_two , (im_two.shape[0] / 8 , im_two.shape[1] / 8))
+im_two[100:200 , 300:450] = 255
+
+#im_one = imresize(im_one , (512 , 512))
+#im_two = imresize(im_two , (512 , 512))
+
+imsave('camel_1.tif' , im_one)
+imsave('camel_2.tif' , im_two)
+
+
+print im_one.shape
+print im_two.shape
+
+
+nnf = nearest_neighbor_field(im_one , im_two , patch_size=(3 , 3))
+
+fixed = apply_nnf(nnf , im_two)
+
+imsave('camel_fixed.tif' , fixed)
+"""
 lena_un = lena()
 
 lena_shifted = generic_filter(lena_un , shift_filter , (101 , 101) , mode='wrap')
@@ -240,3 +320,4 @@ diff_hole = fixed_hole - lena_un
 imsave('lena_hole.tif' , lena_hole)
 imsave('fixed_hole.tif' , fixed_hole)
 imsave('diff_hole.tif' , diff_hole)
+"""
